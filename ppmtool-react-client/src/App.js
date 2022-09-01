50237dd2a31d36a40b4d4a0255cc564cc6a31d15
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-09-01 11:35:59 +0100

    created dashboard, ProjectItem & Header component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Project/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
